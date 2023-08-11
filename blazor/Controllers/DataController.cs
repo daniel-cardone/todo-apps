b6f1860dfc2ef86b8fda20b2c15990a1346aaa1d
daniel-cardone <dcard278@gmail.com>
2023-08-11 19:57:43 -0400

    begin blazor project

---
+++ blazor/Controllers/DataController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blazor.Controllers
+{
+	public class DataController : Controller
+	{
+		[HttpGet]
+		[Route("/tasks/{username}")]
+		public string Tasks(string username)
+		{
+			return username.ToUpper();
+		}
+	}
+}
---
+++ blazor/Globals.cs
@@ -0,0 +1,30 @@
+﻿
+namespace blazor
+{
+	public class Globals
+	{
+		public event Action? Notify;
+
+		private string _username = "";
+		public string Username
+		{
+			get => _username;
+			set
+			{
+				if (_username != value)
+				{
+					_username = value;
+					if (Notify != null)
+					{
+						Notify?.Invoke();
+					}
+				}
+			}
+		}
+
+		public static async Task<string> GetUserTasks(HttpClient Http, string username)
+		{
+            return await Http.GetStringAsync("tasks/" + username);
+		}
+	}
+}
