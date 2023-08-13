804911d87a238e7eefee50574b1bf6454889a523
daniel-cardone <dcard278@gmail.com>
2023-08-13 19:26:07 -0400

    create remaining functionality to complete blazor app

--- blazor/Controllers/DataController.cs
+++ blazor/Controllers/DataController.cs
@@ -1,24 +1,26 @@
 ﻿using blazor.Shared.Templates;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
+using System.Text.Json;
 
 namespace blazor.Controllers
 {
-	public class DataController : Controller
-	{
+    public class DataController : Controller
+    {
         private readonly string _connectionString = "Data Source=Data/userdata.db";
 
         [HttpGet]
-		[Route("/tasks/{username}")]
-		public UserTasks Tasks(string username)
-		{
-			UserTasks userTasks = new();
+        [Route("/tasks/{username}")]
+        public UserTasks GetTasks(string username)
+        {
+            UserTasks userTasks = new();
 
             using (var connection = new SqliteConnection(_connectionString))
             {
                 connection.Open();
 
-                string query = "SELECT * FROM Tasks WHERE Username = @Username";
+                string query = "SELECT * FROM Tasks WHERE Username = @Username ORDER BY Date DESC";
                 using var command = new SqliteCommand(query, connection);
                 command.Parameters.AddWithValue("@Username", username);
                 using var reader = command.ExecuteReader();
@@ -46,5 +48,136 @@ namespace blazor.Controllers
 
             return userTasks;
         }
-	}
+
+        [HttpPost]
+        [Route("/add")]
+        public StatusCodeResult AddTask([FromBody] JsonElement json)
+        {
+            string? username = json.GetString("username");
+            string? taskName = json.GetString("taskName");
+            if (username == null || taskName == null || username.Length > 12 || taskName.Length > 100)
+            {
+                return BadRequest();
+            }
+
+            string taskStatus = "in progress";
+            DateTime date = DateTime.Now;
+            string uuid = Guid.NewGuid().ToString();
+
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO Tasks (Username, Uuid, TaskName, TaskStatus, Date) Values (@Username, @Uuid, @TaskName, @TaskStatus, @Date)";
+                using var command = new SqliteCommand(query, connection);
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Uuid", uuid);
+                command.Parameters.AddWithValue("@TaskName", taskName);
+                command.Parameters.AddWithValue("@TaskStatus", taskStatus);
+                command.Parameters.AddWithValue("@Date", date);
+                int affectedRows = command.ExecuteNonQuery();
+
+                if (affectedRows == 0)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                }
+            }
+
+            return Ok();
+        }
+
+		[HttpPost]
+		[Route("/status")]
+        public StatusCodeResult UpdateTaskStatus([FromBody] JsonElement json)
+        {
+			string? username = json.GetString("username");
+			string? uuid = json.GetString("uuid");
+			string? newStatus = json.GetString("newStatus");
+			if (username == null || uuid == null || username.Length > 12 || (newStatus != "in progress" && newStatus != "complete"))
+			{
+                return BadRequest();
+			}
+
+			using (var connection = new SqliteConnection(_connectionString))
+			{
+				connection.Open();
+
+				string query = "UPDATE Tasks SET TaskStatus = @NewStatus WHERE Username = @Username AND Uuid = @Uuid";
+				using var command = new SqliteCommand(query, connection);
+				command.Parameters.AddWithValue("@NewStatus", newStatus);
+				command.Parameters.AddWithValue("@Username", username);
+				command.Parameters.AddWithValue("@Uuid", uuid);
+				int affectedRows = command.ExecuteNonQuery();
+
+				if (affectedRows == 0)
+				{
+					return BadRequest();
+				}
+			}
+
+			return Ok();
+		}
+
+        [HttpPost]
+        [Route("/rename")]
+        public StatusCodeResult RenameTask([FromBody] JsonElement json)
+        {
+            string? username = json.GetString("username");
+            string? uuid = json.GetString("uuid");
+            string? newName= json.GetString("newName");
+            if (username == null || uuid == null || newName == null || username.Length > 12 || newName.Length > 100)
+            {
+                return BadRequest();
+            }
+
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "UPDATE Tasks SET TaskName = @NewName WHERE Username = @Username AND Uuid = @Uuid";
+                using var command = new SqliteCommand(query, connection);
+                command.Parameters.AddWithValue("@NewName", newName);
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Uuid", uuid);
+                int affectedRows = command.ExecuteNonQuery();
+
+                if (affectedRows == 0)
+                {
+                    return BadRequest();
+                }
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("/delete")]
+        public StatusCodeResult DeleteTask([FromBody] JsonElement json)
+        {
+            string? username = json.GetString("username");
+            string? uuid = json.GetString("uuid");
+            if (username == null || uuid == null || username.Length > 12)
+            {
+                return BadRequest();
+            }
+
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "DELETE FROM Tasks WHERE Username = @Username AND Uuid = @Uuid";
+                using var command = new SqliteCommand(query, connection);
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Uuid", uuid);
+                int affectedRows = command.ExecuteNonQuery();
+
+                if (affectedRows == 0)
+                {
+                    return BadRequest();
+                }
+            }
+
+            return Ok();
+        }
+    }
 }
--- blazor/Globals.cs
+++ blazor/Globals.cs
@@ -1,11 +1,13 @@
-﻿
-using blazor.Shared.Templates;
+﻿using blazor.Shared.Templates;
+using static System.Net.WebRequestMethods;
+using System;
 
 namespace blazor
 {
 	public class Globals
 	{
 		public event Action? Notify;
+		public event Action? SecondaryNotify;
 
 		private string _username = "";
 		public string Username
@@ -16,17 +18,116 @@ namespace blazor
 				if (_username != value)
 				{
 					_username = value;
-					if (Notify != null)
-					{
-						Notify?.Invoke();
-					}
+					InvokeNotifier();
 				}
 			}
 		}
 
-		public static async Task<UserTasks> GetUserTasks(HttpClient Http, string username)
+		private string _currentFilter = "";
+		public string CurrentFilter
+		{
+			get => _currentFilter;
+			set
+			{
+				if (_currentFilter != value)
+				{
+					_currentFilter = value;
+					InvokeNotifier();
+				}
+			}
+		}
+
+		private UserTasks _userTasks = new();
+		public UserTasks UserTasks
+		{
+			get => _userTasks;
+			set
+			{
+				if (_userTasks != value)
+				{
+					_userTasks = value;
+					InvokeNotifier();
+				}
+			}
+		}
+
+		public static async Task<UserTasks> FetchUserTasks(HttpClient Http, string username)
 		{
             return await Http.GetFromJsonAsync<UserTasks>("tasks/" + username) ?? new();
+		}
+
+		public static async Task<bool> AddNewTask(HttpClient Http, string username, string taskName)
+		{
+			var result = await Http.PostAsJsonAsync<object>("add", new
+			{
+				username,
+				taskName
+			});
+
+			if (result.IsSuccessStatusCode)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+        public static async Task<bool> UpdateUserTask(HttpClient Http, string username, string uuid, string newStatus)
+        {
+            var result = await Http.PostAsJsonAsync<object>("status", new
+            {
+                username,
+                uuid,
+				newStatus
+            });
+
+            if (result.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+
+		public static async Task<bool> RenameUserTask(HttpClient Http, string username, string uuid, string newName)
+		{
+            var result = await Http.PostAsJsonAsync<object>("rename", new
+            {
+                username,
+                uuid,
+                newName
+            });
+
+            if (result.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static async Task<bool> DeleteUserTask(HttpClient Http, string username, string uuid)
+		{
+            var result = await Http.PostAsJsonAsync<object>("delete", new
+            {
+                username,
+                uuid
+            });
+
+            if (result.IsSuccessStatusCode)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+		public void InvokeNotifier()
+		{
+			Notify?.Invoke();
+		}
+
+		public void InvokeSecondaryNotifier()
+		{
+			SecondaryNotify?.Invoke();
 		}
 	}
 }
